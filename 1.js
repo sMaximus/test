3a2d43caac79c05915701113debef0f23e22fc1b
zhangjiaming <bjzhangjiaming1@corp.netease.com>
2021-06-02 15:23:27 +0800

    test

--- 1.js
+++ 1.js
@@ -1,30 +1,29 @@
-const math = require('module_name');
-const { methodA } = require('module_name')
+const math = require("module_name");
+const { methodA } = require("module_name");
 console.log(methodA);
-
-var fib = function(n) {
-    if (n < 2) {
-        return n;
-    }
-    let p = 0, q = 0, r = 1;
-    for (let i = 2; i <= n; i++) {
-        p = q;
-        q = r;
-        r = p + q;
-    }
-    return r;
+console.log(methodA);
+var fib = function (n) {
+  if (n < 2) {
+    return n;
+  }
+  let p = 0,
+    q = 0,
+    r = 1;
+  for (let i = 2; i <= n; i++) {
+    p = q;
+    q = r;
+    r = p + q;
+  }
+  return r;
 };
 
-
-
-
 var fib = function (n) {
-  
   if (n < 2) {
     return n;
   }
 
-  let p = 0; q = 0, r = 1;
+  let p = 0;
+  (q = 0), (r = 1);
 
   for (let i = 2; i <= n; i++) {
     p = q;
@@ -32,13 +31,7 @@ var fib = function (n) {
     r = p + q;
   }
 
-  return r
+  return r;
 };
 
-let mergeTwoList = (l1, l2) => {
-  
-
-  
-}
-
-
+let mergeTwoList = (l1, l2) => {};
