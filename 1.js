975d5f8abaa25a6b5d954ea5e2e2b6605de0ad82
zhangjiaming <bjzhangjiaming1@corp.netease.com>
2021-06-02 15:18:50 +0800

    test

---
+++ 1.js
@@ -0,0 +1,44 @@
+const math = require('module_name');
+const { methodA } = require('module_name')
+console.log(methodA);
+
+var fib = function(n) {
+    if (n < 2) {
+        return n;
+    }
+    let p = 0, q = 0, r = 1;
+    for (let i = 2; i <= n; i++) {
+        p = q;
+        q = r;
+        r = p + q;
+    }
+    return r;
+};
+
+
+
+
+var fib = function (n) {
+  
+  if (n < 2) {
+    return n;
+  }
+
+  let p = 0; q = 0, r = 1;
+
+  for (let i = 2; i <= n; i++) {
+    p = q;
+    q = r;
+    r = p + q;
+  }
+
+  return r
+};
+
+let mergeTwoList = (l1, l2) => {
+  
+
+  
+}
+
+
